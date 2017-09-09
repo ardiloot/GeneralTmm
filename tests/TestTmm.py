96ae68578a26f3878388ef445e0100aaf039dec1
Ardi <ardi.loot@ut.ee>
2017-09-09 15:32:28 +0300

    Added tests
    - Tests C++ version of TMM against Python code. Python code has been
    manually tested.

--- tests/TestTmm.py
+++ tests/TestTmm.py
@@ -1,21 +1,41 @@
 import unittest
 import numpy as np
-from GeneralTmm import Tmm, TmmPy
-from LabPy import Material
+from GeneralTmm import Tmm, TmmPy, Material
+
+# Refractive index data of silver from
+# P. B. Johnson and R. W. Christy. Optical Constants of the Noble Metals, Phys. Rev. B 6, 4370-4379 (1972)
+
+wlsAg = np.array([4.0e-07,   4.2e-07,   4.4e-07,   4.6e-07, 4.8e-07,   5.0e-07,
+    5.2e-07,   5.4e-07, 5.6e-07,   5.8e-07,   6.0e-07,   6.2e-07, 6.4e-07,
+    6.6e-07,   6.8e-07,   7.0e-07, 7.2e-07,   7.4e-07,   7.6e-07,   7.8e-07,
+    8.0e-07,   8.2e-07,   8.4e-07,   8.6e-07, 8.8e-07,   9.0e-07,   9.2e-07,
+    9.4e-07, 9.6e-07,   9.8e-07])
+    
+nsAg = np.array([0.050 + 2.104j, 0.046 + 2.348j, 0.040 + 2.553j, 0.044 + 2.751j,
+    0.050 + 2.948j, 0.050 + 3.131j, 0.050 + 3.316j, 0.057 + 3.505j, 0.057 + 3.679j,
+    0.051 + 3.841j, 0.055 + 4.010j, 0.059 + 4.177j, 0.055 + 4.332j, 0.050 + 4.487j,
+    0.045 + 4.645j, 0.041 + 4.803j, 0.037 + 4.960j, 0.033 + 5.116j, 0.031 + 5.272j,
+    0.034 + 5.421j, 0.037 + 5.570j, 0.040 + 5.719j, 0.040 + 5.883j, 0.040 + 6.048j,
+    0.040 + 6.213j, 0.040 + 6.371j, 0.040 + 6.519j, 0.040 + 6.667j, 0.040 + 6.815j,
+    0.040 + 6.962j])
 
 class TmmTest(unittest.TestCase):
     
     @classmethod
     def _PrepareTmm(self, wl, layers):
         tmm = Tmm();
-        tmm.SetParam(wl = wl)
+        tmm.SetParams(wl = wl)
         for layer in layers:
             if layer[0] == "iso":
                 _, d, n = layer
-                tmm.AddIsotropicLayer(d, n)
+                mat = Material.Static(n)
+                tmm.AddIsotropicLayer(d, mat)
             else:
                 _, d, n1, n2, n3, psi, xi = layer
-                tmm.AddLayer(d, n1, n2, n3, psi, xi)
+                matx = Material.Static(n1)
+                maty = Material.Static(n2)
+                matz = Material.Static(n3)
+                tmm.AddLayer(d, matx, maty, matz, psi, xi)
 
         # Old Tmm
         oldTmm = TmmPy()
@@ -45,8 +65,8 @@ class TmmTest(unittest.TestCase):
     def _testFields(self, wl, beta, layers, xs, pol):
         tmm, oldTmm = self._PrepareTmm(wl, layers)
         
-        tmm.SetParam(beta = beta)
-        E, H = tmm.CalcFields1D(xs, pol)
+        tmm.beta = beta
+        E, H = tmm.CalcFields1D(xs, np.array(pol))
         oldTmm.Solve(wl, beta)
         EOld, HOld = oldTmm.CalcFields1D(xs, pol)
 
@@ -71,7 +91,7 @@ class TmmTest(unittest.TestCase):
                         self._testSweep(wl, layers, betas, pol, -1, 0.0)
 
     def testSppSweep(self):
-        silver = Material(r"main/Ag\Johnson")
+        silver = Material(wlsAg, nsAg)
         for wl in [400e-9, 500e-9, 800e-9]:
             for pol in [(1.0, 0.0), (0.0, 1.0)]:
                 print("SPP test", wl, pol)
@@ -106,7 +126,7 @@ class TmmTest(unittest.TestCase):
 
 
     def testSppFields(self):
-        silver = Material(r"main/Ag\Johnson")
+        silver = Material(wlsAg, nsAg)
         beta = 0.5
         xs = np.linspace(-1e-6, 1e-6, 1000)
         for wl in [400e-9, 500e-9, 800e-9]:
